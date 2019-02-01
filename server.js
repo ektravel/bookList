315a295a32701c0aa1630f1f74d58c72f3e2eaa7
ektravel <38017980+ektravel@users.noreply.github.com>
2019-02-01 17:37:24 -0600

    added basing routing and initial class constructor

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
-const express = requrie("express");
+const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
 app.get("/", function(req,res){
-    res.send("Welcome to my Book List!");
+    res.send("Welcome to the list of books I have listened to since 2018!");
 });
 
-app.listen(3000);
+class Book{
+    constructor(title, author, rating){
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+    }
+};
+
+let book1 = new Book("The Path: What Chinese Philosophers Can Teach Us About the Good Life", "Michael Puett", 2 );
+
+let book2 = new Book("A World Lit Only by Fire", "William Manchester", 3);
+
+let book3 = new Book("How Not to Die", "Michael Greger", 3);
+
+app.get("/book1", function(req,res){
+    res.json(book1);
+});
+
+app.get("/book2", function(req,res){
+    res.json(book2);
+});
+
+app.get("/book3", function(req,res){
+    res.json(book3);
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT "+ PORT);
+});
