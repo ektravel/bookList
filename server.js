1aa1bb79f1d81da4fbf2500f397ace44008e1be3
ektravel <38017980+ektravel@users.noreply.github.com>
2019-02-02 21:42:38 -0600

    added body-parser; added basic routing logic; added new book function

--- server.js
+++ server.js
@@ -1,36 +1,55 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
+const app = express();
 const PORT = 3000;
 
-app.get("/", function(req,res){
-    res.send("Welcome to the list of books I have listened to since 2018!");
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 class Book{
     constructor(title, author, rating){
         this.title = title;
         this.author = author;
         this.rating = rating;
-    }
+    }  
 };
 
-let book1 = new Book("The Path: What Chinese Philosophers Can Teach Us About the Good Life", "Michael Puett", 2 );
+let books = [];
 
-let book2 = new Book("A World Lit Only by Fire", "William Manchester", 3);
+function addBook(title, author, rating){
+    let book = new Book(title, author, rating);
+    books.push(book);
+};
 
-let book3 = new Book("How Not to Die", "Michael Greger", 3);
+addBook("The Path: What Chinese Philosophers Can Teach Us About the Good Life", "Michael Puett", 2 );
 
-app.get("/book1", function(req,res){
-    res.json(book1);
+addBook("A World Lit Only by Fire", "William Manchester", 3);
+
+addBook("How Not to Die", "Michael Greger", 3);
+
+app.get("/", function(req,res){
+    res.send("All the books I have listened to since 2018!");
 });
 
-app.get("/book2", function(req,res){
-    res.json(book2);
+app.get("/api/:books?", function(req,res){
+    let listing = req.params.books;
+    
+    if(listing){
+        for (let i = 0; i < books.length; i++){
+            if (listing === books[i].author){
+                return res.json(books[i]);
+            }
+        }
+        return res.send("No author found"); 
+    }
+    return res.json(books);
 });
 
-app.get("/book3", function(req,res){
-    res.json(book3);
+app.post("/api/books", function(req, res){
+    let newBook = req.body;
+    books.push(newBook);
+    res.json(newBook);
 });
 
 app.listen(PORT, function(){
