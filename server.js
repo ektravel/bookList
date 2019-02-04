e9ee412b5c7986616ae25b126fc1e29388e72c34
ektravel <38017980+ektravel@users.noreply.github.com>
2019-02-04 16:53:07 -0600

    added all.html page to display all books

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -29,24 +29,25 @@ addBook("A World Lit Only by Fire", "William Manchester", 3);
 
 addBook("How Not to Die", "Michael Greger", 3);
 
+//basic route that sends the user to the AJAX page
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/api/:books?", function(req,res){
-    let listing = req.params.books;
-    
-    if(listing){
-        for (let i = 0; i < books.length; i++){
-            if (listing === books[i].author){
-                return res.json(books[i]);
-            }
-        }
-        return res.json(false); 
-    }
+app.get("/add", function(req,res){
+    res.sendFile(path.join(_dirname, "add.html"));
+});
+
+app.get("/all", function(req,res){
+    res.sendFile(path.join(_dirname, "all.html"));
+});
+
+//display all books
+app.get("/api/books", function(req,res){
     return res.json(books);
 });
 
+//add new book
 app.post("/api/books", function(req, res){
     let newBook = req.body;
     books.push(newBook);
